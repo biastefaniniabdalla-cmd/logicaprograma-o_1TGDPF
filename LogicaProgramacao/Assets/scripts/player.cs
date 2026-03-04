28aa605acc1b4a7fd69fc883717ad9f7b7a62f56
biastefaniniabdalla-cmd <biastefaniniabdalla@gmail.com>
2026-03-04 11:57:58 -0300

    hello world

---
+++ LogicaProgramacao/Assets/scripts/player.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Unity");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Hello, World");
+    }
+}
